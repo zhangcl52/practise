1f300ca2e8267c47aa255ab551ad8016d4fd9c28
zhangcl52 <zhangcl52@ziroom.com>
2018-07-24 14:14:32 +0800

    学习ThreadLocal类

---
+++ src/main/java/com/hunter/study/javabase/jdk8/ListDemo.java
@@ -0,0 +1,25 @@
+package com.hunter.study.javabase.jdk8;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by zhangcl
+ * Data: 2018/7/17  12:27
+ * version 1.0
+ */
+public class ListDemo {
+    public static void main(String[] args) {
+        List<Integer> list = Lists.newArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.stream().filter(n -> (
+                n == 3)
+        ).collect(Collectors.toList());
+        list.stream().forEach(n -> System.out.println(n));
+    }
+}
